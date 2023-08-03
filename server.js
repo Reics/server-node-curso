9f1b0b4ccc60329667967ed8e569f4e779bfc0a2
Salvador Acosta <salvador.acosta@inegi.org.mx>
2023-08-03 15:19:09 -0600

    Cambios para Heroku

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 const hbs = require('hbs');
-require('./hbs/helpers')
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -23,6 +25,6 @@ app.get('/about', function (req, res) {
     res.render('about')
 });
 
-app.listen(3000, () =>{
-    console.log('Escuchando peticiones en el puerto 3000');
+app.listen(port, () =>{
+    console.log(`Escuchando peticiones en el puerto ${port}`);
 });
